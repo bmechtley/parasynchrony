1a8dcaa691be3ac27604e2ab522c6d2632176419
plant <bmechtley@gmail.com>
2015-01-20 11:50:37 -0600

    new method signatures for models package

--- metrics_browser/server.py
+++ metrics_browser/server.py
@@ -1,33 +1,24 @@
 import itertools
 import StringIO
+import json
 import sys
 import os
+import PIL
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-from PIL import Image
 
 import numpy as np
 import matplotlib.cm
 import matplotlib.colors
 
 import flask
-
 from lru import LRU
 
 import models
 
-
 app = flask.Flask(__name__)
-
-model = models.Parasitism.get_model("nbd(2)")
-covariance = np.array([
-    [1E-1, 1E-2, 0, 0],
-    [1E-2, 1E-2, 0, 0],
-    [0, 0, 1E-1, 1E-2],
-    [0, 0, 1E-2, 1E-1]
-])
-
+model = models.parasitism.get_model("nbd(2)")
 hcache = LRU(100)
+
 
 @app.route('/')
 def index():
@@ -49,54 +40,202 @@ def get_image(hhash, row, col):
         flask.abort(404)
 
 
+def transfer_function(params=None, domain=None, **_):
+    sym_params = {
+        models.parasitism.symbols[k]: v for k, v in params.iteritems()
+    }
+
+    xfer = model.transfer_function(sym_params)
+
+    h = np.array([
+        [
+            xfer(complex(re, im))
+            for re in domain[1]
+        ]
+        for im in domain[0]
+    ])
+
+    logmagh = np.log(np.abs(h) + np.finfo(h.dtype).eps)
+
+    color_norm = matplotlib.colors.Normalize(
+        vmin=np.amin(logmagh[logmagh != np.amin(logmagh)]),
+        vmax=np.amax(logmagh)
+    )
+
+    pcolor = matplotlib.cm.cubehelix(color_norm(logmagh))
+
+    return pcolor
+
+
+def fraction_synchrony(
+        params=None,
+        domain=None,
+        axes=None,
+        noise=None,
+        nfreqs=100,
+        **_
+):
+    """
+    Compute the fraction of synchrony between patches for which the host is
+    responsible using different metrics (fraction of spectrum averages,
+    fraction of spectrum maximums).
+
+    TODO: Allow caching of spectra.
+    TODO: Return fraction for which different populations are responsible.
+    TODO: Optimize.
+    TODO: Allow for different parameter ranges.
+
+    :param params: model parameter dictionary.
+    :param dims: number of datapoints along each dimension.
+    :param axes: over which parameters to iterate.
+    :param noise: noise parameters (H/P variance, H-H/P-P covariance)
+    :param resolution: number of frequency values to compute.
+    :return: dictionary of metric: fraction pairs.
+    """
+
+    fracfuns = dict(avg=np.mean, max=np.amax)
+
+    results = {
+        k: np.zeros((
+            len(domain[0]),
+            len(domain[1]),
+            len(model.vars),
+            len(model.vars)
+        ))
+        for k in fracfuns
+    }
+
+    variants = {key: dict(
+        params=dict(params.iteritems()),
+        noise=dict(noise.iteritems())
+    ) for key in ['host', 'nohost']}
+
+    variants['nohost']['params']['mh'] = 0
+    variants['nohost']['noise']['Shh'] = 0
+
+    for (i, var1), (j, var2) in itertools.product(
+        enumerate(domain[0]), enumerate(domain[1])
+    ):
+        for name, variant in variants.iteritems():
+            for k, v in itertools.izip(
+                    [ax['param'] for ax in axes], [var1, var2]
+            ):
+                if k in variant['params']:
+                    variant['params'][k] = v
+                if k in variant['noise']:
+                    variant['noise'][k] = v
+
+            if name == 'nohost':
+                variant['params']['mh'] = 0
+                variant['noise']['Shh'] = 0
+
+            sym_params = {
+                models.parasitism.params[k]: v
+                for k, v in variant['params'].iteritems()
+            }
+
+            noisecov = np.array([
+                [variant['noise']['Sh'], variant['noise']['Shh'], 0, 0],
+                [variant['noise']['Shh'], variant['noise']['Sh'], 0, 0],
+                [0, 0, variant['noise']['Sp'], variant['noise']['Spp']],
+                [0, 0, variant['noise']['Spp'], variant['noise']['Sp']]
+            ])
+
+            variant['spectrum'] = np.abs(np.array([model.calculate_spectrum(
+                sym_params, noisecov, freq
+            ) for freq in np.linspace(0, 0.5, nfreqs)]))
+
+            variant['covariance'] = model.calculate_covariance(
+                sym_params, noisecov
+            )
+
+            variant['normed'] = np.rollaxis(np.array([
+                [
+                    np.real(variant['spectrum'][:, row, col]) / np.sqrt(
+                        np.prod([
+                            variant['covariance'][c, c]**2 for c in [row, col]
+                        ])
+                    ) for col in range(variant['covariance'].shape[1])
+                ] for row in range(variant['covariance'].shape[0])
+            ]), 2, 0)
+
+        for key, fracfun in fracfuns.iteritems():
+            results[key][i, j, :, :] = np.log(fracfun(
+                np.divide(
+                    variants['host']['normed'] - variants['nohost']['normed'],
+                    variants['nohost']['normed']
+                ),
+                axis=0
+            ))
+
+    color_norms = {
+        k: matplotlib.colors.Normalize(
+            vmin=np.amin(result), vmax=np.amax(result)
+        ) for k, result in results.iteritems()
+    }
+
+    return {
+        k: np.array([
+            [
+                matplotlib.cm.cubehelix(color_norms[k](result[:, :, col, row]))
+                for col in range(result.shape[3])
+            ] for row in range(result.shape[2])
+        ]).swapaxes(0, 2).swapaxes(1, 3)
+        for k, result in results.iteritems()
+    }
+
+
 @app.route('/pcolor.json')
 def get_pcolor():
-    params = {k: flask.request.args.get(k) for k in [
-        'r', 'a', 'c', 'k', 'mh', 'mp'
-    ]}
-    axes = {k: flask.request.args.get(k) for k in ['axis_x', 'axis_y']}
-    dims = {k: flask.request.args.get(k) for k in ['width', 'height']}
+    request = flask.request.args
 
-    hhash = str(hash(frozenset(
-        sorted(params.items()) + sorted(axes.items()) + sorted(dims.items())
-    )))
+    inputs = dict(
+        params={
+            k: float(request.get(k))
+            for k in ['r', 'a', 'c', 'k', 'mh', 'mp']
+        },
+        axes=[
+            dict(
+                param=request.get('axis_%s' % ax),
+                min=float(request.get('axis_%s_min' % ax)),
+                max=float(request.get('axis_%s_max' % ax)),
+                n=int(request.get('axis_%s_n' % ax))
+            ) for ax in ['x', 'y']
+        ],
+        noise={k: float(request.get(k)) for k in ['Shh', 'Sh', 'Spp', 'Sp']}
+    )
 
-    print "<pcolor> Loading transfer function %s." % hhash
+    hhash = str(hash(json.dumps(inputs, sort_keys=True)))
+
+    inputs['domain'] = [
+        np.linspace(ax['min'], ax['max'], ax['n'])
+        for ax in inputs['axes'][::-1]
+    ]
+
+    metric_name = request.get('metric')
+    metric = dict(
+        xfer=transfer_function,
+        avgsync=lambda **kw: fraction_synchrony(**kw)['avg'],
+        maxsync=lambda **kw: fraction_synchrony(**kw)['max']
+    )[metric_name]
+
+    print "<pcolor> Loading metric %s, %s." % (metric_name, hhash), inputs
 
     if hhash not in hcache.keys():
         print "<pcolor>\tComputing new."
-        sym_params = {
-            models.Parasitism.params[k]: v for k, v in params.iteritems()
-        }
-        xfer = model.transfer_function(sym_params)
 
-        valrange_x = np.linspace(-1, 1, dims.get('width', 100))
-        valrange_y = np.linspace(-1, 1, dims.get('height', 100))
-
-        h = np.array([
-            [
-                xfer(complex(re, im))
-                for re in valrange_x
-            ]
-            for im in valrange_y
-        ])
-
-        logmagh = np.log(np.abs(h) + np.finfo(h.dtype).eps)
-
-        color_norm = matplotlib.colors.Normalize(
-            vmin=np.amin(logmagh[logmagh != np.amin(logmagh)]),
-            vmax=np.amax(logmagh)
-        )
-
-        pcolor = matplotlib.cm.cubehelix(color_norm(logmagh))
+        pcolor = metric(**inputs)
+        print 'pcolor shape', pcolor.shape
 
         hcache[hhash] = dict(
             images=np.array([
                 [
-                    Image.fromarray(np.uint8(pcolor[:, :, row, col] * 255))
-                    for col in range(h.shape[3])
+                    PIL.Image.fromarray(
+                        np.uint8(pcolor[:, :, row, col, :].swapaxes(0, 1) * 255)
+                    )
+                    for col in range(pcolor.shape[3])
                 ]
-                for row in range(h.shape[2])
+                for row in range(pcolor.shape[2])
             ], dtype=object)
         )
 
