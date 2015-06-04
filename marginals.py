5db1986c5b20ce17aad45104e949ab3e66efda8d
plant <bmechtley@gmail.com>
2015-06-04 04:04:04 -0500

    added qsub job

--- marginals.py
+++ marginals.py
@@ -504,7 +504,7 @@ def generate_runs(config, runtype='qsub'):
             '#PBS -d %s\n' % os.getcwd(),
             '#PBS -e /users/mechtley/logs/%s.err\n' % config['file']['name'],
             '#PBS -o /users/mechtley/logs/%s.out\n' % config['file']['name'],
-            '#PBS -J 0-%d\n' % ((ncalcs + 1) / slice_size),
+            '#PBS -t 0-%d\n' % ((ncalcs + 1) / slice_size),
             ' '.join([
                 'python marginals.py run',
                 '%s (${PBS_ARRAY_INDEX}*%d) (${PBS_ARRAY_INDEX}*%d+%d)\n' % (
