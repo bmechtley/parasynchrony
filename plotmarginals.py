b567ee0c0b241b7aac60052eab6cbcb72f0bf19a
plant <bmechtley@gmail.com>
2015-06-05 10:47:57 -0500

    marginal plots etc.

--- plotmarginals.py
+++ plotmarginals.py
@@ -37,15 +37,18 @@ def plot_marginals(
     gathered = cPickle.load(open('%s-full.pickle' % cacheprefix))
 
     popkey, effectkey = 'h', 'Rhh'
-    hists = gathered['counts'][popkey][effectkey]
 
     varkeys, paramkeys = [config['props'][k] for k in 'varkeys', 'paramkeys']
     fig = pp.figure(figsize=(len(varkeys) * 15, len(varkeys) * 10))
 
-    for sampkey in config['samplings']:
+    for sampkey in config['args']['samplings']:
         for spi, ((vki1, vk1), (vki2, vk2)) in enumerate(
             itertools.combinations_with_replacement(enumerate(varkeys), 2)
         ):
+            print '\t', sampkey
+
+            hists = gathered['counts'][sampkey][popkey][effectkey]
+
             ax = fig.add_subplot(
                 len(varkeys),
                 len(varkeys),
@@ -56,111 +59,116 @@ def plot_marginals(
             vk1r, vk2r = [config['params'][vkn]['range'] for vkn in vk1, vk1]
 
             sampling = config['args']['samplings'][sampkey]
-            samprange = sampling['range']
             sampres = sampling['resolution']
+            samprange = sampling['range']
 
-            hist = np.array(hists[sampkey][vki1, vki2], dtype=float)
-            histf = hist.flatten()
+            if len(samprange) > 1:
+                hist = np.array(hists[vki1, vki2], dtype=float)
+                histf = hist.flatten()
 
-            zs = np.linspace(float(samprange[0]), float(samprange[1]), sampres)
+                zs = np.linspace(float(samprange[0]), float(samprange[1]), sampres)
 
-            mx, my, mz = np.meshgrid(vk2r, vk1r, zs)
-            mxf, myf, mzf = [a.flatten() for a in mx, my, mz]
+                mx, my, mz = np.meshgrid(vk2r, vk1r, zs)
+                mxf, myf, mzf = [a.flatten() for a in mx, my, mz]
 
-            pzs = np.tile(
-                np.array([
-                    scipy.stats.percentileofscore(histf, score) for score in zs
-                ])[np.newaxis, np.newaxis, :],
-                (len(vk2r), len(vk1r), 1)
-            )
-            pzsf = pzs.flatten()
-
-            mxf, myf, mzf, pzsf, histf = [
-                a[histf != 0] for a in mxf, myf, mzf, pzsf, histf
-            ]
-
-            cmap = matplotlib.cm.get_cmap('jet')
-            maxcount = float(np.amax(hist))
-
-            ax.set_xlim(np.amin(vk2r), np.amax(vk2r))
-            ax.set_ylim(np.amin(vk1r), np.amax(vk1r))
-            ax.set_zlim(np.amin(mzf), np.amax(mzf))
-            ax.set_xlabel(vk2)
-            ax.set_ylabel(vk1)
-            ax.set_zlabel('%s / %s' % (popkey, effectkey))
-
-            if show_marginals:
-                xmy, xmz = np.meshgrid(vk1r, zs)
-                xmx = np.sum(hist, axis=0).T / np.amax(hist)
-                ax.contourf(
-                    xmx, xmy, xmz,
-                    zdir='x',
-                    offset=vk2r[0] - np.diff(vk2r)[0] / 10.0,
-                    cmap=cmap,
-                    alpha=0.5
+                pzs = np.tile(
+                    np.array([
+                        scipy.stats.percentileofscore(histf, score) for score in zs
+                    ])[np.newaxis, np.newaxis, :],
+                    (len(vk2r), len(vk1r), 1)
                 )
+                pzsf = pzs.flatten()
 
-                ymx, ymz = np.meshgrid(vk2r, zs)
-                ymy = np.sum(hist, axis=1).T / np.amax(hist)
-                ax.contourf(
-                    ymx, ymy, ymz,
-                    zdir='y',
-                    offset=vk1r[-1] + np.diff(vk1r)[-1] / 10.0,
-                    cmap=cmap,
-                    alpha=0.5
-                )
+                mxf, myf, mzf, pzsf, histf = [
+                    a[histf != 0] for a in mxf, myf, mzf, pzsf, histf
+                ]
 
-                zmx, zmy = np.meshgrid(vk2r, vk1r)
-                zmz = np.sum(hist, axis=2) / np.amax(hist)
-                ax.contourf(
-                    zmx, zmy, zmz,
-                    zdir='z',
-                    offset=zs[0] - np.diff(zs)[0] / 10.0,
-                    cmap=cmap,
-                    alpha=0.5
-                )
+                cmap = matplotlib.cm.get_cmap('jet')
+                maxcount = float(np.amax(hist))
 
-            if show_bins:
-                ax.scatter(
-                    mxf, myf, mzf,
-                    s=np.log10(1 + histf / maxcount) * 1000,
-                    c=cmap(pzsf / np.amax(pzsf)),
-                    lw=0,
-                    alpha=1.0
-                )
+                ax.set_xlim(np.amin(vk2r), np.amax(vk2r))
+                ax.set_ylim(np.amin(vk1r), np.amax(vk1r))
+                ax.set_zlim(np.amin(mzf), np.amax(mzf))
+                ax.set_xlabel(vk2)
+                ax.set_ylabel(vk1)
+                ax.set_zlabel('%s / %s' % (popkey, effectkey))
 
-            cumsums = np.cumsum(hist, axis=2)
-            cumsums /= np.amax(cumsums, axis=2)[:, :, np.newaxis]
-
-            if percentiles is not None and len(percentiles):
-                percentiles = np.array(percentiles) / 100
-                colors = cmap(percentiles)
-
-                for perc, color in zip(percentiles, colors):
-                    vals = np.interp(
-                        np.array([
-                            [
-                                np.searchsorted(cumsums[vk1d, vk2d], perc)
-                                for vk2d in range(cumsums.shape[1])
-                            ]
-                            for vk1d in range(cumsums.shape[0])
-                        ]),
-                        [0, sampling['resolution'] - 1],
-                        sampling['range']
+                if show_marginals:
+                    print '\t\tmarginals'
+                    xmy, xmz = np.meshgrid(vk1r, zs)
+                    xmx = np.sum(hist, axis=0).T / np.amax(hist)
+                    ax.contourf(
+                        xmx, xmy, xmz,
+                        zdir='x',
+                        offset=vk2r[0] - np.diff(vk2r)[0] / 10.0,
+                        cmap=cmap,
+                        alpha=0.5
                     )
 
-                    ax.plot_trisurf(
-                        zmx.flatten(),
-                        zmy.flatten(),
-                        vals.flatten(),
-                        color=color,
-                        alpha=0.5,
-                        label=perc
+                    ymx, ymz = np.meshgrid(vk2r, zs)
+                    ymy = np.sum(hist, axis=1).T / np.amax(hist)
+                    ax.contourf(
+                        ymx, ymy, ymz,
+                        zdir='y',
+                        offset=vk1r[-1] + np.diff(vk1r)[-1] / 10.0,
+                        cmap=cmap,
+                        alpha=0.5
                     )
 
-                ax.legend()
+                    zmx, zmy = np.meshgrid(vk2r, vk1r)
+                    zmz = np.sum(hist, axis=2) / np.amax(hist)
+                    ax.contourf(
+                        zmx, zmy, zmz,
+                        zdir='z',
+                        offset=zs[0] - np.diff(zs)[0] / 10.0,
+                        cmap=cmap,
+                        alpha=0.5
+                    )
 
-        pp.savefig('%s-%s.png' % (cacheprefix, sampkey))
+                if show_bins:
+                    print '\t\tbins'
+                    ax.scatter(
+                        mxf, myf, mzf,
+                        s=np.log10(1 + histf / maxcount) * 1000,
+                        c=cmap(pzsf / np.amax(pzsf)),
+                        lw=0,
+                        alpha=1.0
+                    )
+
+                cumsums = np.cumsum(hist, axis=2)
+                cumsums /= np.amax(cumsums, axis=2)[:, :, np.newaxis]
+
+                if percentiles is not None and len(percentiles):
+                    print '\t\tpercentiles'
+
+                    percentiles = np.array(percentiles) / 100
+                    colors = cmap(percentiles)
+
+                    for perc, color in zip(percentiles, colors):
+                        vals = np.interp(
+                            np.array([
+                                [
+                                    np.searchsorted(cumsums[vk1d, vk2d], perc)
+                                    for vk2d in range(cumsums.shape[1])
+                                ]
+                                for vk1d in range(cumsums.shape[0])
+                            ]),
+                            [0, sampling['resolution'] - 1],
+                            sampling['range']
+                        )
+
+                        ax.plot_trisurf(
+                            zmx.flatten(),
+                            zmy.flatten(),
+                            vals.flatten(),
+                            color=color,
+                            alpha=0.5,
+                            label=perc
+                        )
+
+                    ax.legend()
+
+            pp.savefig('%s-%s.png' % (cacheprefix, sampkey))
 
 def main():
     """Main."""
